5648bc223450ffb42945a104774590d1126a33a8
Jasper Helmich <99034085@mydavinci.nl>
2017-05-18 13:55:27 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 Route::resource('home', 'homecontroller');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'homecontroller@index')->name('home');
