91c32c9a7b63a431860aedd8919e56ed9961fc1c
Jasper Helmich <99034085@mydavinci.nl>
2017-06-14 09:57:03 +0200

    Fixed the collapse

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -384,35 +384,23 @@
 <!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
-
 <!-- jQuery 2.2.3 -->
 <script src="{{ asset('/css/adminlte/plugins/jQuery/jquery-2.2.3.min.js')}}"></script>
-<script src="{{ asset('/css/adminlte/dist/js/app.min.js')}}"></script>
+<!-- Bootstrap 3.3.6 -->
 <script src="{{ asset('/css/adminlte/bootstrap/js/bootstrap.min.js')}}"></script>
-<script src="{{ asset('/css/adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{ asset('/css/adminlte/plugins/datatables/dataTables.bootstrap.min.js')}}"></script>
-<!-- SlimScroll -->
-<script src="{{ asset('/css/adminlte/plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
 <!-- FastClick -->
 <script src="{{ asset('/css/adminlte/plugins/fastclick/fastclick.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('/css/adminlte/dist/js/app.min.js')}}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('/css/adminlte/dist/js/demo.js')}}"></script>
-<!-- page script -->
-<!-- Ckeditor -->
-<script src="{{asset('/css/adminlte/plugins/ckeditor/ckeditor.js')}}"></script>
-<!-- FastClick -->
-<script src="{{ asset('/css/adminlte/plugins/fastclick/fastclick.js')}}"></script>
 <!-- Sparkline -->
 <script src="{{ asset('/css/adminlte/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
+<!-- jvectormap -->
+<script src="{{ asset('/css/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+<script src="{{ asset('/css/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<!-- SlimScroll 1.3.0 -->
+<script src="{{ asset('/css/adminlte/plugins/slimScroll/jquery.slimscroll.min.js')}}"></script>
 <!-- ChartJS 1.0.1 -->
 <script src="{{ asset('/css/adminlte/plugins/chartjs/Chart.min.js')}}"></script>
-
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. Slimscroll is required when using the
-     fixed layout. -->
      <script>
     CKEDITOR.replace( 'editor1' );
 </script>
