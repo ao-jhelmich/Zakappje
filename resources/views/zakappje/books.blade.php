6f0f789ddb3babbd644a94f08c628b2e6c25c34d
Jasper Helmich <99034085@mydavinci.nl>
2017-06-29 13:45:33 +0200

    Style book read

--- resources/views/zakappje/books.blade.php
+++ resources/views/zakappje/books.blade.php
@@ -4,22 +4,49 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        ZakAppje
+        Klasse eis
+        <small>Mainrequirement</small>
       </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Work</a></li>
+        <li><a href="#">to</a></li>
+        <li class="active">do</li>
+      </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
-      @isset ($requirement)
-          <h1>{{$requirement->requirements_name}}<a href="/check/{{$requirement->requirements_id}}/{{Auth::user()->id}}">
-          <button class="btn bg-olive margin pull-right">Aftekenen</button></a>
-          </h1>
+
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">{{$requirement->requirements_name}}</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            @if(Auth::check())
+              <a href="/check/{{$requirement->requirements_id}}/{{Auth::user()->id}}">
+              <button class="btn bg-olive margin pull-right">Aftekenen</button></a>
+            @endif
+          </div>
+        </div>
+        <div class="box-body">
           @isset ($instructions)  
             @foreach ($instructions as $instruction)
               <div class="pre">{!!htmlspecialchars_decode($instruction->instructions_desc)!!}</div> 
             @endforeach             
           @endisset
-      @endisset
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+          possible admin info (last updated created BY and on)
+        </div>
+        <!-- /.box-footer-->
+      </div>
+      <!-- /.box -->
+
     </section>
+    <!-- /.content -->
   </div>
 @endsection
