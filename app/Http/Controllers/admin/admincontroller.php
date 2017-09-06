576ff3c6decf7d8da72686647372231a71d7e0a8
Jasper Helmich <99034085@mydavinci.nl>
2017-09-06 13:53:05 +0200

    Added better admin security

--- app/Http/Controllers/admin/admincontroller.php
+++ app/Http/Controllers/admin/admincontroller.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 use App\mainrequirements;
 use App\Instructions;
--- app/User.php
+++ app/User.php
@@ -28,4 +28,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function hasRole($role)
+    {
+        return User::where('accountRole', $role)->get();
+    }
 }
