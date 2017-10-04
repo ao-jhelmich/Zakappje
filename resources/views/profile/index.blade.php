52c1101c3135ff0d649b1f910c6385b35da3cf45
Dylan de Leeuw <dylan.deleeuw@live.nl>
2017-10-04 21:53:55 +0200

    profile edit
    werkt alleen nog maar als alles word ingevuld

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -26,6 +26,10 @@
                 <div class="box-body box-profile">
               <img class="profile-user-img img-responsive img-circle" src="/images/user8-128x128.jpg" alt="User profile picture">
               <h3 class="profile-username text-center">{{$profile->name .' '. $profile->lastName}}</h3>
+
+                          <a data-toggle="modal" data-target="#editModel" href="#">edit</a>
+
+
               <p class="text-muted text-center">
               @if (isset(Auth::user()->accountRole))
                 @if (Auth::user()->accountRole == 1)
@@ -44,8 +48,13 @@
                   <b>Email</b> <a class="pull-right">{{$profile->email}}</a>
                 </li>
                 <li class="list-group-item">
-                  <b>Adres gegevens</b> <a class="pull-right">{{$profile->streetAdress ." ". $profile->houseNumber 
-                    ." ". $profile->postal_code .' '. $profile->city}}</a>
+                  <b>Adres</b> <a class="pull-right">{{$profile->streetAdress ." ". $profile->houseNumber}}</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Woonplaats</b> <a class="pull-right">{{$profile->city}}</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Postcode</b> <a class="pull-right">{{$profile->postal_code}}</a>
                 </li>
                 <li class="list-group-item">
                   <b>Eigen telefoon nummer</b> <a class="pull-right">{{$profile->user_phone_number}}</a>
@@ -83,7 +92,10 @@
                   <b>Geboorte datum</b> <a class="pull-right">{{$profile->birth_day_day ." ". $month ." ".  $profile->birth_day_year}}</a>
                 </li>
                 <li class="list-group-item">
-                  <b>Ouder gegevens</b> <a class="pull-right">{{$profile->user_parent_name . " " . $profile->user_parent_email}}</a>
+                  <b>Ouder naam</b> <a class="pull-right">{{$profile->user_parent_name}}</a>
+                </li>
+                <li class="list-group-item">
+                  <b>Ouder email</b> <a class="pull-right">{{$profile->user_parent_email}}</a>
                 </li>
                 <li class="list-group-item">
                   <b>Ouder telefoon</b> <a class="pull-right">{{$profile->user_parent_phone}}</a>
@@ -97,4 +109,190 @@
           <!-- /.nav-tabs-custom -->
     </section>
 </div>
+@yield('content')
+<div class="modal fade" id="editModel" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Edit</h4>
+      </div>
+      <div class="modal-body">
+
+          {{ Form::open(['url' => '/edit-user', 'method' => 'Post']) }}
+          <div class="form-group">
+              
+
+                        {{ csrf_field() }}
+          </div>
+          
+                    
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Naam:</label>
+
+                            <div class="col-md-6">
+                              {{ Form::text('name', $profile->name, array('class' => 'form-control')) }}
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('lastName') ? ' has-error' : '' }}">
+                            <label for="lastName" class="col-md-4 control-label">Achternaam:</label>
+
+                            <div class="col-md-6">
+                                {{ Form::text('lastName', $profile->lastName, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('lastName'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('lastName') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">e-mail adres</label>
+
+                            <div class="col-md-6">
+                                {{ Form::email('email', $profile->email, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('city') ? ' has-error' : '' }}">
+                            <label for="city" class="col-md-4 control-label">Stad</label>
+
+                            <div class="col-md-6">
+                                {{ Form::text('city', $profile->city, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('city'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('city') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('streetAdress') ? ' has-error' : '' }}">
+                            <label for="streetAdress" class="col-md-4 control-label">Adres + Huisnummer</label>
+
+                            <div class="col-md-6">
+                                
+                                {{ Form::text('streetAdress', $profile->streetAdress, array('class' => 'form-control', 'style' => 'width: 80%; float: left; margin-right: 2%')) }}
+                                {{ Form::text('houseNumber', $profile->houseNumber, array('class' => 'form-control', 'style' => 'width: 18%;')) }}
+
+                                @if ($errors->has('streetAdress'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('streetAdress') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('postal_code') ? ' has-error' : '' }}">
+                            <label for="postal_code" class="col-md-4 control-label">Postcode</label>
+
+                            <div class="col-md-6">
+                                {{ Form::text('postal_code', $profile->postal_code, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('postal_code'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('postal_code') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        
+                        <div class="form-group{{ $errors->has('user_phone_number') ? ' has-error' : '' }}">
+                            <label for="user_phone_number" class="col-md-4 control-label">Je eigen nummer</label>
+
+                            <div class="col-md-6">
+                                {{ Form::number('user_phone_number', $profile->user_phone_number, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('user_phone_number'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_phone_number') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('birth_day') ? ' has-error' : '' }}">
+                            <label for="birth_day" class="col-md-4 control-label">Geboorte datum:</label>
+
+                            <div class="col-md-6">
+                                {{ Form::date('birth_day', $profile->birth_day, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('birth_day_year'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('birth_day') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="form-group{{ $errors->has('user_parent_phone') ? ' has-error' : '' }}">
+                            <label for="user_parent_phone" class="col-md-4 control-label">Ouder telefoon nummer:</label>
+
+                            <div class="col-md-6">
+                                {{ Form::number('user_parent_phone', $profile->user_parent_phone, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('user_parent_phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_parent_phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('user_parent_name') ? ' has-error' : '' }}">
+                            <label for="user_parent_name" class="col-md-4 control-label">Ouder naam:</label>
+
+                            <div class="col-md-6">
+                                {{ Form::text('user_parent_name', $profile->user_parent_name, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('user_parent_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_parent_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('user_parent_email') ? ' has-error' : '' }}">
+                            <label for="user_parent_email" class="col-md-4 control-label">Ouder e-mail:</label>
+
+                            <div class="col-md-6">
+                                {{ Form::email('user_parent_email', $profile->user_parent_email, array('class' => 'form-control')) }}
+
+                                @if ($errors->has('user_parent_email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('user_parent_email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {{ Form::submit('Opslaan', array('class' => 'btn btn-primary'))}}
+                            </div>
+                        </div>
+                        {{ Form::close() }}
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>  
 @endsection
